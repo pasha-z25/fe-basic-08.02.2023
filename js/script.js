b4124f62b1a4c6d92b7222709edd8cacb663d2a1
Pasha Zubak <pashazubak@gmail.com>
2023-04-18 00:07:07 +0300

    add responsive

--- js/script.js
+++ js/script.js
@@ -1,16 +1,55 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('#slick').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     infinite: true,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
   $('#owl').owlCarousel({
     loop: true,
     nav: false,
+    dots: true,
     margin: 40,
     items: 4,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1250: {
+        items: 4,
+      },
+    },
   });
 });
-	
