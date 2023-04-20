1ddea8a4429ddaf745225d5b7050c2f12ab8c27c
Pasha Zubak <pashazubak@gmail.com>
2023-04-20 12:56:58 +0300

    add slider check

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
+const slickSlider = document.getElementById('slick');
+const owlCarousel = document.getElementById('owl');
+
 $(document).ready(function () {
-  $('#slick').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    infinite: true,
-    arrows: false,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 750,
-        settings: {
-          slidesToShow: 1,
+  if (slickSlider) {
+    $('#slick').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: false,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 1,
+          },
+        }
+      ],
+    });
+  }
+
+  if (owlCarousel) {
+    $('#owl').owlCarousel({
+      loop: true,
+      nav: false,
+      dots: true,
+      margin: 40,
+      items: 4,
+      responsive: {
+        0: {
+          items: 1,
+          margin: 20,
         },
-      }
-    ],
-  });
-  $('#owl').owlCarousel({
-    loop: true,
-    nav: false,
-    dots: true,
-    margin: 40,
-    items: 4,
-    responsive: {
-      0: {
-        items: 1,
-        margin: 20,
+        750: {
+          items: 4,
+          margin: 40,
+        }
       },
-      750: {
-        items: 4,
-        margin: 40,
-      }
-    },
-  });
+    });
+  }
 });
